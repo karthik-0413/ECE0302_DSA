57af2d672873bff5c38aea12eee8ae42a856c903
Karthik Raja <kar304@pitt.edu>
2024-02-13 21:20:47 -0500

    Finished Problem 04

--- programming_problems/problem04/postpre/algebraic_expressions.cpp
+++ programming_problems/problem04/postpre/algebraic_expressions.cpp
@@ -2,6 +2,7 @@
 using std::string;
 
 #include <iostream>
+#include <stack>
 
 #include <cctype> // for isalpha
 
@@ -41,8 +42,38 @@ bool isPost(string s) {
   return (firstChar == 0);
 }
 
-void convert(string &postfix, string &prefix) {
+void convert(string &postfix, string &prefix)
+{
+  // Creating a stack from the C++ STL
+  std::stack<std::string> s;
 
-  // TODO
-  
+  // For-Loop to check each character in the string
+  for(int i = 0; i < postfix.length(); i++)
+  {
+    // If the character is an operator
+    if(isoperator(postfix[i]))
+    {
+      // We have to evaluate and push the result
+      // First operator and pop it out of stack
+      std::string op1 = s.top();
+      s.pop();
+      // Second operator and pop it out of stack
+      std::string op2 = s.top();
+      s.pop();
+      // Push result into the stack
+      std::string op3 = postfix[i] + op2 + op1;
+      s.push(op3);
+    }
+    // If the character is an operand
+    else
+    {
+      // Need to convert the char into a string, so I am concatenting
+      // it with an empty string
+      std::string final = "";
+      // Push the value of the operand into the stack
+      s.push(final + postfix[i]);
+    }
+  }
+  // The prefix resulting value will be the top-most value in the stack
+  prefix = s.top();
 }
